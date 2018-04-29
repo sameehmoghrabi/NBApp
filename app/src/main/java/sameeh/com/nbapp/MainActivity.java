3812fe9237677607d42640ec5b41fa42fa26fe86
Sameeh <sameeh.moghrabi@gmail.com>
2018-04-30 02:38:10 +0300

    First commit

---
+++ app/src/main/java/sameeh/com/nbapp/MainActivity.java
@@ -0,0 +1,13 @@
+package sameeh.com.nbapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
